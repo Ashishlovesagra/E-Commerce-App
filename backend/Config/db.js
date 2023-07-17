debb32bffb68a29d709c3448f2fe1efaed2e2643
ashish <ashishlovesagra@gmail.com>
2023-07-18 01:03:13 +0530

    check url

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -3,6 +3,7 @@ import  Color  from 'colors';
 
 const connectDB = async () => {
     try {
+        console.log(process.env.MONGO_URL)
         const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`Connected To MongoDb Database ${conn.connection.host}`.bgGreen.black)
     } catch (error) {
